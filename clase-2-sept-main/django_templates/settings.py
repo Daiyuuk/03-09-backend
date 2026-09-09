c14a07a935145cd1c7f535a4a73097d24b89fa2d
Laboratorios Chillan <chilab@inacap.cl>
2026-09-09 15:35:19 -0300

    Solucion

--- clase-2-sept-main/django_templates/settings.py
+++ clase-2-sept-main/django_templates/settings.py
@@ -58,7 +58,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Directorio donde buscar plantillas
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
